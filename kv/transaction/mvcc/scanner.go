4e40593d25a74e9cb3038838ac1d7ada2b3ecd6d
250313341 <suohailongjob@163.com>
2023-03-23 23:46:32 +0800

    complete mvcc scanner code

--- kv/transaction/mvcc/scanner.go
+++ kv/transaction/mvcc/scanner.go
@@ -1,8 +1,6 @@
 package mvcc
 
 import (
-	"bytes"
-
 	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/log"
 )
@@ -12,48 +10,70 @@ import (
 // Invariant: either the scanner is finished and cannot be used, or it is ready to return a value immediately.
 type Scanner struct {
 	// Your Data Here (4C).
-	startKey []byte
-	txn      *MvccTxn
+	iter engine_util.DBIterator
+	next []byte
+	txn  *MvccTxn
 }
 
 // NewScanner creates a new scanner ready to read from the snapshot in txn.
 func NewScanner(startKey []byte, txn *MvccTxn) *Scanner {
 	// Your Code Here (4C).
+	iter := txn.Reader.IterCF(engine_util.CfWrite)
 	return &Scanner{
-		startKey: startKey,
-		txn:      txn,
+		iter: iter,
+		next: startKey,
+		txn:  txn,
 	}
 }
 
 func (scan *Scanner) Close() {
 	// Your Code Here (4C).
-	scan.txn.Reader.Close()
+	scan.iter.Close()
 }
 
 // Next returns the next key/value pair from the scanner. If the scanner is , then it will return `nil, nil, nil`.
 func (scan *Scanner) Next() ([]byte, []byte, error) {
 	// Your Code Here (4C).
-	iter := scan.txn.Reader.IterCF(engine_util.CfWrite)
-	defer iter.Close()
-	iter.Seek(EncodeKey(scan.startKey, scan.txn.StartTS))
-	for ; iter.Valid(); iter.Next() {
-		item := iter.Item()
-		userKey := DecodeUserKey(item.Key())
-		if bytes.Equal(key, userKey) {
-			value, err := item.Value()
-			if err != nil {
-				log.Errorf("")
-				return nil, err
-			}
-			write, err := ParseWrite(value)
-			if err != nil {
-				log.Errorf("")
-				return nil, err
-			}
-			if write.Kind == WriteKindPut {
-				return txn.Reader.GetCF(engine_util.CfDefault, EncodeKey(key, write.StartTS))
-			}
-		}
+	// defer iter.Close()
+	if scan.next == nil {
+		return nil, nil, nil
 	}
+
+	key := scan.next
+	scan.iter.Seek(EncodeKey(key, scan.txn.StartTS))
+	if !scan.iter.Valid() {
+		scan.next = nil
+		return nil, nil, nil
+	}
+
+	// 获取当前值
+	item := scan.iter.Item()
+	currentKey := DecodeUserKey(item.Key())
+	currentTs := decodeTimestamp(item.Key())
+	for scan.iter.Valid() && currentTs > scan.txn.StartTS {
+		scan.iter.Seek(EncodeKey(currentKey, scan.txn.StartTS))
+		item := scan.iter.Item()
+		currentKey = DecodeUserKey(item.Key())
+		currentTs = decodeTimestamp(item.Key())
+
+	}
+
+	// if bytes.Equal(item.Key(), userKey) {
+	value, err := item.Value()
+	if err != nil {
+		log.Errorf("")
+		return nil, nil, err
+	}
+	write, err := ParseWrite(value)
+	if err != nil {
+		log.Errorf("")
+		return nil, nil, err
+	}
+	if write.Kind == WriteKindPut {
+		v, err := scan.txn.Reader.GetCF(engine_util.CfDefault, EncodeKey(item.Key(), write.StartTS))
+		return userKey, v, err
+	}
+	// }
+	iter.Next()
 	return nil, nil, nil
 }
